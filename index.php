7dcd6632914f834bb51bcefd8db3b3cd2fe025dd
webmiss54 <webmiss54@gmail.com>
2017-09-24 15:57:33 +0200

    previnform

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
@@ -12,22 +12,28 @@
 	<title></title>
       <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.js"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="js/carroussel.js">
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <link href='//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' rel='stylesheet'/>
+
 </head>
 <body>
 <header>
-<img src="images/logo-prev-in-Form.png">
+<img class="entete" src="images/logo-prev-in-Form.png">
 </header>
 
-    <nav class="navbar navbar-inverse">
+    <nav class="navbar navbar-inverse navbar-fixed bg-primary">
   <div class="container-fluid">
     <div class="navbar-header">
       <!--<a class="navbar-brand" href="#">WebSiteName</a>-->
@@ -39,7 +45,7 @@
         <span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="#">Historique de l'entreprise</a></li>
-          <li><a href="#">Présentation de l'équipe adminsitrative</a></li>
+          <li><a href="#">Présentation de l'équipe administrative</a></li>
           <li><a href="#">Agréments et habilitations</a></li>
         </ul>
       </li>
@@ -47,9 +53,17 @@
         <a class="dropdown-toggle" data-toggle="dropdown" href="#">Nos formations
         <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="#">Planning Formation</a></li>
-          <li><a href="#">Présentation de l'équipe adminsitrative</a></li>
-          <li><a href="#">Agréments et habilitations</a></li>
+          <li><a href="#">CACES</a></li>
+          <li><a href="#">Sauveteurs Secouristes</a></li>
+          <li><a href="#">Habilitations electriques</a></li>
+          <li><a href="#">Incendie/évacuation</a></li>
+          <li><a href="#">Manutention ferroviaire</a></li>
+          <li><a href="#">Domaines Techniques</a></li>
+          <li><a href="#">Santé et sécurité au travail</a></li>
+          <li><a href="#">Bureautique</a></li>
+          <li><a href="#">Qualité dans le monde du travail</a></li>
+          <li><a href="#">Management</a></li>
+          <li><a href="#">Formations avec nos partenaires</a></li>
         </ul>
       </li> 
       <li><a href="#">Planning et documentation</a></li>
@@ -57,35 +71,225 @@
         <a class="dropdown-toggle" data-toggle="dropdown" href="#">Notre savoir-faire
         <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="#">Planning Formation</a></li>
-          <li><a href="#">Présentation de l'équipe adminsitrative</a></li>
-          <li><a href="#">Agréments et habilitations</a></li>
-        </ul>
+          <li><a href="#">Matériel de Production</a></li>
+          <li><a href="#">Expertise des formations</a></li>
+          <li><a href="#">Nos références clients</a></li>
+          <li><a href="#">Qualités</a></li>
+        </ul>      
+        <li><a href="#">Actualites et salons</a></li>
+      <li><a href="#">Livraisons et logistique</a></li>
       </li><li class="dropdown">
         <a class="dropdown-toggle" data-toggle="dropdown" href="#">Contactez nous
         <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="#">Plan d'accès/a></li>
+          <li><a href="#">Carriere et emplois</li>
           <li><a href="#">demande de documentation</a></li>
-          <li><a href="#">Agréments et habilitations</a></li>
+          
         </ul>
       </li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
+
     </ul>
   </div>
 </nav>
-    <div class="container">
-
-      <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
-
-    </div><!-- /.container -->
 
 
+<div class="container">
+          
+     	<div id="myCarousel" class="carousel slide col-md-8" data-ride="carousel">
+                        
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                            
+                <div class="item active">
+                                
+                    <img src="./images/DSC00002.JPG">
+                    <div class="carousel-caption">
+                    <h4><a href="#">Lorem ipsum dolor sit amet consetetur sadipscing</a></h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
+                    eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+                    invidunt ut labore et dolore magna aliquyam erat. 
+                    <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">
+                    Free Bootstrap Carousel Collection</a></p>
+                                    
+                    </div>            
+                </div><!-- End Item -->
+ 
+                <div class="item">
+                    <img src="./images/DSC00015.JPG">
+                    <div class="carousel-caption">
+                    <h4><a href="#">consetetur sadipscing elitr, sed diam nonumy eirmod</a></h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
+                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum 
+                    dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
+                    labore et dolore magna aliquyam erat. 
+                    <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">
+                    Free Bootstrap Carousel Collection</a></p>
+                    </div>
+                </div><!-- End Item -->
+        
+                <div class="item">
+                            
+                    <img src="./images/DSC00038.JPG">
+                    <div class="carousel-caption">
+                    <h4><a href="#">tempor invidunt ut labore et dolore</a></h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+                    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, 
+                    consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. 
+                    <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">
+                    Free Bootstrap Carousel Collection</a></p>
+                    </div>
+                                
+                </div><!-- End Item -->
 
+                 <div class="item">
+                            
+                    <img src="./images/DSC00072.JPG">
+                    <div class="carousel-caption">
+                    <h4><a href="#">tempor invidunt ut labore et dolore</a></h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+                    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, 
+                    consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. 
+                    <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">
+                    Free Bootstrap Carousel Collection</a></p>
+                    </div>
+                                
+                </div><!-- End Item -->
+                 <div class="item">
+                            
+                    <img src="./images/DSC00117.JPG">
+                    <div class="carousel-caption">
+                    <h4><a href="#">tempor invidunt ut labore et dolore</a></h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+                    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, 
+                    consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. 
+                    <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">
+                    Free Bootstrap Carousel Collection</a></p>
+                    </div>
+                                
+                </div><!-- End Item -->
+                
+   			</div><!-- End Carousel Inner -->
+   						
+   			<ul class="list-group col-md-3 carousel-indicators">
+               
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1" ></li>
+                    <li data-target="#myCarousel" data-slide-to="2" ></li>
+                    <li data-target="#myCarousel" data-slide-to="3" ></li> 
+                    <li data-target="#myCarousel" data-slide-to="4" ></li>
+               
+    		</ul>
+        </div><!-- End Carousel -->
+                         
+    </div><!-- End Tab one -->
+</div>
+
+
+<footer>
+<section class="nb-footer">
+<div class="container">
+<div class="row">
+<div class="col-md-3 col-sm-6">
+	<div class="footer-single">
+	<!-- 	<img src="images/logo.png" class="img-responsive" alt="Logo"> -->
+
+		<!-- This is only for better view of theme if you want your image logo remove div dummy-logo bellow and replace your logo in logo.png and uncomment logo tag above-->
+	<div class="dummy-logo">
+	<div class="icon pull-left brand">
+		<i class="fa fa-copy"></i>
+	</div><div class="footer-single">
+		<div class="footer-title"><h2>Nos Agréments</h2></div>
+		<ul class="list-unstyled agrement">
+			<li>CACES® cariste R389 certification SGS N°ICSCAC0043</li>
+			<li>CACES® PEMP R386 certificat SGS N°ICSCAC0043</li>
+			<li>CAUES PONT par la CARSAT AM convention  N°1540900021</li>
+			<li>SST par la CARSAT AM convention N°2078/202/SST-1/15</li>
+			<li>CHSCT par ministère du travail, de l’emploi, de la formation professionnelle et du dialogue social.</li>
+		</ul>
+	</div></div>
+	<!--	<a href="" class="btn btn-footer">Read More</a>-->
+	</div>
+</div>
+
+<div class="col-md-3 col-sm-6">
+	<div class="footer-single useful-links">
+	 <div class="footer-title"><h2>Navigation</h2></div>
+	 <ul class="list-unstyled">
+            <li><a href="index.html">Acceuil <i class="fa fa-angle-right pull-right"></i></a></li>
+            <li><a href="about1.html">Qui sommes nous ? <i class="fa fa-angle-right pull-right"></i></a></li>
+            <li><a href="service1.html">Nos formations <i class="fa fa-angle-right pull-right"></i></a></li>
+            <li><a href="portfolio1.html">Planning et documentations <i class="fa fa-angle-right pull-right"></i></a></li>
+            <li><a href="pricing.html">Notre Savoir-Faire <i class="fa fa-angle-right pull-right"></i></a></li>
+            <li><a href="contact1.html">Contactez nous <i class="fa fa-angle-right pull-right"></i></a></li>
+       </ul>
+        </div>
+</div>
+<div class="clearfix visible-sm"></div>
+
+<div class="col-md-3 col-sm-6">
+	
+	<div class="col-sm-12 left-clear right-clear footer-single footer-project">
+		<div class="footer-title"><h2>Labels</h2></div>
+		<div class="col-xs-6">
+			<img src="./images/logo-caces-sgs.jpg" class="img-responsive center-block">
+		</div>
+		<div class="col-xs-6">
+			<img src="./images/Qualicert_Def.jpg" class="img-responsive center-block">
+		</div>
+		<!--<div class="col-xs-4">
+			<img src="http://lorempixel.com/200/200/" class="img-responsive center-block">
+		</div>
+		<div class="clearfix"></div>
+		<div class="col-xs-4">
+			<img src="http://lorempixel.com/200/200/" class="img-responsive center-block">
+		</div>
+		<div class="col-xs-4">
+			<img src="http://lorempixel.com/200/200/" class="img-responsive center-block">
+		</div>
+		<div class="col-xs-4">
+			<img src="http://lorempixel.com/200/200/" class="img-responsive center-block">
+		</div>-->
+
+	</div>
+
+</div>
+
+<div class="col-md-3 col-sm-6">
+	<div class="footer-single">
+		<div class="footer-title"><h2>Contact Information</h2></div>
+		<address>
+			35, Hameau des Trois Sources<br>
+			57700 HAYANGE LE KONACKER <br>
+			<i class="fa fa-phone"></i> +33 3.82.86.67.00<br>
+			<i class="fa fa-envelope"></i> contact@prev-in-form.fr<br>
+		</address>					
+	</div>
+</div>
+
+</div>
+</div>
+</section>	
+
+<section class="nb-copyright">
+<div class="container">
+<div class="row">
+<div class="col-sm-6 copyrt xs-center">
+	2017 © All Rights Reserved. <a href="">Terms & Conditions</a> | <a href="">Privacy Policy</a>
+</div>
+<div class="col-sm-6 text-right xs-center">
+	<ul class="list-inline footer-social">
+		<li><a href="https://www.facebook.com/prev.in.form/"><i class="fa fa-facebook"></i></a></li>
+		<li><a href=""><i class="fa fa-twitter"></i></a></li>
+		<li><a href=""><i class="fa fa-youtube-play"></i></a></li>
+		<li><a href=""><i class="fa fa-google-plus"></i></a></li>
+		<li><a href=""><i class="fa fa-skype"></i></a></li>
+	</ul>
+</div>
+</div>
+</div>
+</section>
+</footer>
 
 </body>
 </html>
